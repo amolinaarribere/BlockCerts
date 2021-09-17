4862768e16af0565f60329a52bb4455c57c983bb
Alberto Molina Arribere <amolina@vaudoise.ch>
2021-09-17 16:54:48 +0200

    menu

--- src/Demo.js
+++ src/Demo.js
@@ -5,6 +5,7 @@ import Box from '@material-ui/core/Box';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import { Container, Row, Col } from 'react-bootstrap';
 
 
 import PublicComponent from './components/PublicComponent.js';
@@ -60,7 +61,8 @@ class Demo extends React.Component {
  }
 
   state = {
-    value : 0
+    value : 0,
+    isMenuShown : true
   };
 
   
@@ -68,24 +70,38 @@ class Demo extends React.Component {
     const handleChange = (event, newValue) => {
       this.setState({value: newValue});
     };
+    const toggleMenu = () => {
+      if(this.state.isMenuShown)this.setState({ isMenuShown: false })
+      else this.setState({ isMenuShown: true })
+    };
 
     return (
-      <div style={(loadFunc.Network == "ropsten")? ({backgroundColor: 'DarkGray'}) :
-                  (loadFunc.Network == "rinkeby")? ({backgroundColor: 'AntiqueWhite'}) :
-                  (loadFunc.Network == "kovan")? ({backgroundColor: 'Azure'}) : 
-                  (loadFunc.Network == "main")? ({backgroundColor: 'Lavender'}) : {backgroundColor: 'White'}}>
+      <div style={{backgroundColor: 'White'}}>
         <AppBar position="static">
-          <Tabs value={this.state.value} onChange={handleChange} aria-label="simple tabs example">
-            <Tab label="Manager" {...a11yProps(0)} />
-            <Tab label="Public" {...a11yProps(1)} />
-            <Tab label="Private" {...a11yProps(2)} />
-            <Tab label="Provider" {...a11yProps(3)} />
-            <Tab label="Price Converter" {...a11yProps(4)} />
-            {certFunc.isOwner ? (<Tab label="Treasury" {...a11yProps(5)} />) : null}
-            {certFunc.isOwner ? (<Tab label="Certis Tokens" {...a11yProps(6)} />) : null}
-          </Tabs>
+          <Container>
+            <Row>
+              <Col class="mx-auto w-25">
+                <button class="nav-brand" onClick={toggleMenu}>
+                  <img class="img-responsive" src="https://cdn2.iconfinder.com/data/icons/camping-outline-flat-pt-2/100/069_-_snowboard_snowboarding_board_1-128.png"  alt="logo"/>
+                </button>
+              </Col>
+              <Col class="mx-auto w-75">
+                <h1>BLOCKCERTS {loadFunc.Network}</h1>
+              </Col>
+            </Row>
+          </Container>  
+          {(this.state.isMenuShown) ? (
+            <Tabs value={this.state.value} onChange={handleChange} aria-label="simple tabs example">
+              <Tab label="Manager" {...a11yProps(0)} />
+              <Tab label="Public" {...a11yProps(1)} />
+              <Tab label="Private" {...a11yProps(2)} />
+              <Tab label="Provider" {...a11yProps(3)} />
+              <Tab label="Price Converter" {...a11yProps(4)} />
+              {certFunc.isOwner ? (<Tab label="Treasury" {...a11yProps(5)} />) : null}
+              {certFunc.isOwner ? (<Tab label="Certis Tokens" {...a11yProps(6)} />) : null}
+            </Tabs>
+        ) : null}
         </AppBar>
-        <h1>{loadFunc.Network} NETWORK</h1>  
         <CurrentAddressComponent />
         <br />
         <TabPanel value={this.state.value} index={0}>
