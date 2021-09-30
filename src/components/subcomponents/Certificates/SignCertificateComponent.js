d4d9ca6c6df032d76e3cb8ecf3768e7e537fcb08
Alberto Molina Arribere <amolina@vaudoise.ch>
2021-09-30 15:51:35 +0200

    signature v4 PoC added

--- src/components/subcomponents/Certificates/SignCertificateComponent.js
+++ src/components/subcomponents/Certificates/SignCertificateComponent.js
@@ -4,6 +4,8 @@ import { Form, Container, Row, Col } from 'react-bootstrap';
 const func = require("../../../functions/CertificateFunctions.js");
 const Aux = require("../../../functions/AuxiliaryFunctions.js");
 const Contracts = require("../../../functions/Contracts.js");
+const sigUtil = require("eth-sig-util");
+const ethUtil = require('ethereumjs-util');
 
 class SignCertificateComponent extends React.Component{
 
@@ -106,11 +108,120 @@ class SignCertificateComponent extends React.Component{
       
       this.resetState()
   };
+
+  handleSignCertificate2 = async (event) => {
+    event.preventDefault();
+    const msgParams = JSON.stringify({
+      domain: {
+        // Defining the chain aka Rinkeby testnet or Ethereum Main Net
+        chainId: await Aux.web3.eth.getChainId(),
+        // Give a user friendly name to the specific contract you are signing for.
+        name: 'Ether Mail',
+        // If name isn't enough add verifying contract to make sure you are establishing contracts with the proper entity
+        verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
+        // Just let's you know the latest version. Definitely make sure the field name is correct.
+        version: '1',
+      },
+  
+      // Defining the message signing data content.
+      message: {
+        /*
+         - Anything you want. Just a JSON Blob that encodes the data you want to send
+         - No required fields
+         - This is DApp Specific
+         - Be as explicit as possible when building out the message schema.
+        */
+        contents: 'Hello, Bob!',
+        attachedMoneyInEth: 4.2,
+        from: {
+          name: 'Cow',
+          wallets: [
+            '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
+            '0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF',
+          ],
+        },
+        to: [
+          {
+            name: 'Bob',
+            wallets: [
+              '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
+              '0xB0BdaBea57B0BDABeA57b0bdABEA57b0BDabEa57',
+              '0xB0B0b0b0b0b0B000000000000000000000000000',
+            ],
+          },
+        ],
+      },
+      // Refers to the keys of the *types* object below.
+      primaryType: 'Mail',
+      types: {
+        // TODO: Clarify if EIP712Domain refers to the domain the contract is hosted on
+        EIP712Domain: [
+          { name: 'name', type: 'string' },
+          { name: 'version', type: 'string' },
+          { name: 'chainId', type: 'uint256' },
+          { name: 'verifyingContract', type: 'address' },
+        ],
+        // Not an EIP712Domain definition
+        Group: [
+          { name: 'name', type: 'string' },
+          { name: 'members', type: 'Person[]' },
+        ],
+        // Refer to PrimaryType
+        Mail: [
+          { name: 'from', type: 'Person' },
+          { name: 'to', type: 'Person[]' },
+          { name: 'contents', type: 'string' },
+        ],
+        // Not an EIP712Domain definition
+        Person: [
+          { name: 'name', type: 'string' },
+          { name: 'wallets', type: 'address[]' },
+        ],
+      },
+    });
+  
+    var from = Aux.account;
+  
+    var params = [from, msgParams];
+    var method = 'eth_signTypedData_v4';
+  
+    Aux.web3.currentProvider.sendAsync(
+      {
+        method,
+        params,
+        from,
+      },
+      function (err, result) {
+        if (err) return console.dir(err);
+        if (result.error) {
+          alert(result.error.message);
+        }
+        if (result.error) return console.error('ERROR', result);
+        console.log('TYPED SIGNED:' + JSON.stringify(result.result));
+  
+        const recovered = sigUtil.recoverTypedSignature_v4({
+          data: JSON.parse(msgParams),
+          sig: result.result,
+        });
+  
+        if (
+          ethUtil.toChecksumAddress(recovered) === ethUtil.toChecksumAddress(from)
+        ) {
+          alert('Successfully recovered signer as ' + from);
+        } else {
+          alert(
+            'Failed to verify signer when comparing ' + result + ' to ' + from
+          );
+        }
+      }
+    );
+  }
+    
   
     render(){
         return (
           <div>
-            <Form onSubmit={this.handleSignCertificate} style={{margin: '50px 50px 50px 50px' }}>
+            <Form onSubmit={this.handleSignCertificate2} style={{margin: '50px 50px 50px 50px' }}>
               <Form.Group controlId="formFile" className="mb-3">
                 <Form.Control type="file" onChange={this.captureFile}/>
                 <Form.Control type="text" name="HolderAddress" placeholder="holder address" 
